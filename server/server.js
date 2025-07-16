ea0f2f91dba85475b37a5afc48e5e9b46b926183
kelly brevin <kellybrevin83@gmail.com>
2025-07-16 13:50:30 +0300

    new commits

--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ const PORT = process.env.PORT || 5000;
 //connect to mongoDB
 connectDB();
 
+const userRoutes = require("./routes/userRoutes");
+app.use("/api/users", userRoutes);
+
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
 });
