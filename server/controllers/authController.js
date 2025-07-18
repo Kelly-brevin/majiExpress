bf746b0474c2aca0397bf036b21879bcc34d255d
kelly brevin <kellybrevin83@gmail.com>
2025-07-18 14:33:06 +0300

    new commit

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,3 +3,21 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 //register a new user
+exports.register = async (req, res) => {
+  const { name, email, phoneNumber, location, password } = req.body;
+};
+try {
+  //check for existing useer
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return res.status(400).json({ message: "email already registered" });
+  }
+  const newUser = await User.create({
+    name,
+    email,
+    phoneNumber,
+    location,
+    password,
+  });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+} catch (error) {}
