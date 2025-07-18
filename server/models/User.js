354fd361cc104d23dca15dc039608f3610a4231e
kelly brevin <kellybrevin83@gmail.com>
2025-07-18 13:58:44 +0300

    new commits

--- server/models/User.js
+++ server/models/User.js
@@ -20,7 +20,20 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: false,
     },
+    password: {
+      type: String,
+      required: true,
+      minLenghth: 6,
+    },
   },
   { timestamps: true }
 );
+
+//hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 module.exports = mongoose.model("User", userSchema);
