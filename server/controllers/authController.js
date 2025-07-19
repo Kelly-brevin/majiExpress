e6adc2ec1066d2c769f0094fd7dc00eba5bb3654
kelly brevin <kellybrevin83@gmail.com>
2025-07-19 20:00:47 +0300

    new commit

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -33,5 +33,33 @@ try {
     },
   });
 } catch (err) {
-  res.status(500).json({message:"server error",error:error.message})
+  res.status(500).json({ message: "server error", error: error.message });
 }
+//login user
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    //find user by email
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "invalid credentials" });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        phoneNumber: user.phoneNumber,
+        location: user.location,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
