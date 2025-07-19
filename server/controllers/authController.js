f18d5b6df499633b72f74096d3b98297bc6bcfaa
kelly brevin <kellybrevin83@gmail.com>
2025-07-19 20:21:40 +0300

    commits

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,36 +5,37 @@ const jwt = require("jsonwebtoken");
 //register a new user
 exports.register = async (req, res) => {
   const { name, email, phoneNumber, location, password } = req.body;
-};
-try {
-  //check for existing useer
-  const userExists = await User.findOne({ email });
-  if (userExists) {
-    return res.status(400).json({ message: "email already registered" });
+  try {
+    //check for existing useer
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "email already registered" });
+    }
+    const newUser = await User.create({
+      name,
+      email,
+      phoneNumber,
+      location,
+      password,
+    });
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(201).json({
+      token,
+      user: {
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        phoneNumber: newUser.phoneNumber,
+        location: newUser.location,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ message: "server error", error: err.message });
   }
-  const newUser = await User.create({
-    name,
-    email,
-    phoneNumber,
-    location,
-    password,
-  });
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
-  res.status(201).json({
-    token,
-    user: {
-      id: newUser._id,
-      name: newUser.name,
-      email: newUser.email,
-      phoneNumber: newUser.phoneNumber,
-      location: newUser.location,
-    },
-  });
-} catch (err) {
-  res.status(500).json({ message: "server error", error: error.message });
-}
+};
+
 //login user
 exports.login = async (req, res) => {
   const { email, password } = req.body;
