5a752900723439c94cd4ec159798043979955cdf
kelly brevin <kellybrevin83@gmail.com>
2025-07-22 00:00:44 +0300

    updates

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,3 +1,6 @@
 const express = require("express");
 const router = router();
 const { login, register } = require("../controllers/authController");
+router.post("/register", register);
+router.post("/login", login);
+module.exports = router;
