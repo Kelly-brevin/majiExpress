c636c0f6802cfa27652aa2dcf1ca9fd1e21959c5
kelly brevin <kellybrevin83@gmail.com>
2025-07-22 21:03:14 +0300

    updates

--- server/models/admin.js
+++ server/models/admin.js
@@ -1,4 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const adminSchema = new mongoose.Schema();
+const adminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+//hash password before saving
+adminSchema.pre("save", async function (next) {
+    
+});
