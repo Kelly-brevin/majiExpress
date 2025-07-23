36ae2145bb54b183ab13fadfe68004955f085c79
kelly brevin <kellybrevin83@gmail.com>
2025-07-23 21:18:10 +0300

    new commit

--- server/models/admin.js
+++ server/models/admin.js
@@ -1,6 +1,6 @@
 // const express = require("express");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const adminSchema = new mongoose.Schema({
   name: {
     type: String,
