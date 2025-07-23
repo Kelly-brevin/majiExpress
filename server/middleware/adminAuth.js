3faa16ae21a63dd8831f97a553f41ffda964ae0c
kelly brevin <kellybrevin83@gmail.com>
2025-07-23 23:25:06 +0300

    new commits

--- server/middleware/adminAuth.js
+++ server/middleware/adminAuth.js
@@ -9,7 +9,7 @@ const adminAuth = async (req, res, next) => {
   //retrieve authrorization header from the incoming request
   const authHeader = req.headers.authorization;
 
-  //token must be set as : Authorization:Bearer <token>
+  //token must be set as : Authorization:Bearer <token>.Check for missing or malformed token
   if (!authHeader || !authHeader.startsWith("Bearer")) {
     return res.status(401).json({ message: "No token,authorization denied" });
   }
@@ -17,5 +17,20 @@ const adminAuth = async (req, res, next) => {
   const token = authHeader.split(" ")[1];
 
   try {
-  } catch (error) {}
+    //verify and decode the token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    //fetch admin from DB and attach to request
+    const admin = await Admin.findById(decoded.id).select("-password");
+
+    if (!admin) {
+      return res.status(401).json({ message: "not authorized as admin" });
+    }
+    //attach admin info to request object
+    req.admin = admin;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
 };
+module.exports = adminAuth;
