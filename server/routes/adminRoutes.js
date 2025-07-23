e4338050b2e01d5d4fd353e0b898091ac17211c1
kelly brevin <kellybrevin83@gmail.com>
2025-07-23 21:20:04 +0300

    new commits

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const { registerAdmin, loginAdmin } = require("../controllers/adminController");
 
 //define routes
-router.post("/", registerAdmin);
-router.post("/", loginAdmin);
+router.post("/register", registerAdmin);
+router.post("/login", loginAdmin);
 
 module.exports = router;
