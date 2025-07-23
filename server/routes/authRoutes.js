4446c96c4f66080c670f21f4df3e108d25b781fd
kelly brevin <kellybrevin83@gmail.com>
2025-07-23 23:56:22 +0300

    middleware

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,11 +2,14 @@ const express = require("express"); //this is the commonjs module pattern
 //create a router instance
 const router = express.Router();
 
+//bring in middleware
+const authMiddleware = require("../middlewares/adminAuth");
+
 //destructure to import login and register functions directly
 const { login, register } = require("../controllers/authController");
 
 //define routes
-router.post("/register", register);
+router.post("/register", authMiddleware, register);
 router.post("/login", login);
 //export router to make it available to be mounted in server.js
 module.exports = router;
