a17f1b7d91a71578bd8ca089ce2064da3f50fd73
kelly brevin <kellybrevin83@gmail.com>
2025-07-23 23:15:59 +0300

    new commits

--- server/middleware/adminAuth.js
+++ server/middleware/adminAuth.js
@@ -1,8 +1,21 @@
 //import jsonwebtoken library
 const jwt = require("jsonwebtoken");
+
 //import admin mongoose model
 const Admin = require("../models/admin");
+
 //async middleware function
 const adminAuth = async (req, res, next) => {
+  //retrieve authrorization header from the incoming request
   const authHeader = req.headers.authorization;
+
+  //token must be set as : Authorization:Bearer <token>
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    return res.status(401).json({ message: "No token,authorization denied" });
+  }
+  //extract token part
+  const token = authHeader.split(" ")[1];
+
+  try {
+  } catch (error) {}
 };
