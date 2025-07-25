d1a6b874af808eae074dc1cd51b49e8ef82e6298
kelly brevin <kellybrevin83@gmail.com>
2025-07-25 11:49:44 +0300

    fresh changes

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,5 +1,21 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-    
-});
+const productSchema = new mongoose.Schema(
+  {
+    size: {
+      type: String,
+      required: true,
+      enum: ["1L", "5L", "10L", "20L"],
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    available: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Product", productSchema);
