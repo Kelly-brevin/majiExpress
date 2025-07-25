c505f82696ef1408e1d84e40d4b5e004af472bf1
kelly brevin <kellybrevin83@gmail.com>
2025-07-25 11:59:36 +0300

    model created

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,11 +1,13 @@
+//import mongoose library
 const mongoose = require("mongoose");
 
+//mongoose.Schema() is a constructor
 const productSchema = new mongoose.Schema(
   {
     size: {
-      type: String,
+      type: String, //capitalized 'String' is a js constructor (not string literal)
       required: true,
-      enum: ["1L", "5L", "10L", "20L"],
+      enum: ["1L", "5L", "10L", "20L"], //enum restricts the value of 'size'to one of the specified strings
     },
     price: {
       type: Number,
