5fde3091ce67c381634572d8a9cfccf020807756
kelly brevin <kellybrevin83@gmail.com>
2025-07-27 23:38:23 +0300

    order controller

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -34,3 +34,29 @@ exports.getUserOrders = async (req, res) => {
 };
 
 // admin: get all orders
+exports.getAllOrders = async (_req, res) => {
+  try {
+    const orders = await Order.find()
+      .populate("user")
+      .populate("items.product");
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching all orders" });
+  }
+};
+
+//admin: update order status
+exports.updateOrderStatus = async (req, res) => {
+  try {
+    const order = await Order.findByIdAndUpdate(
+      req.params.id,
+      { status: req.body.status },
+      { new: true, runValidators: true }
+    );
+
+    if (!order) return res.status(404).json({ message: "Order not found" });
+    res.json(order);
+  } catch (error) {
+    res.status(400).json({ message: "Invalid order ID" });
+  }
+};
