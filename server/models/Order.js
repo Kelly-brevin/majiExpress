55e1a4a760fe0abbccd9276063084ba82137da18
kelly brevin <kellybrevin83@gmail.com>
2025-07-27 23:07:57 +0300

    order model

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,8 +1,40 @@
 const mongoose = require("mongoose");
-const orderSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          default: 1,
+        },
+      },
+    ],
+    location: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: "String",
+      enum: ["pending", "in-progress", "delivered"],
+      default: "pending",
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["paid", "unpaid"],
+      default: "unpaid",
+    },
   },
-});
+  { timestamps: true }
+);
+module.exports = mongoose.model("Order", orderSchema);
