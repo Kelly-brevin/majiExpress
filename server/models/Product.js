46650953c8812174570f565d09df1200d91689c9
kelly brevin <kellybrevin83@gmail.com>
2025-07-29 13:09:34 +0300

    new commits

--- server/models/Product.js
+++ server/models/Product.js
@@ -7,7 +7,7 @@ const productSchema = new mongoose.Schema(
     size: {
       type: String, //capitalized 'String' is a js constructor (not string literal)
       required: true,
-      enum: ["1L", "5L", "10L", "20L"], //enum restricts the value of 'size'to one of the specified strings
+      enum: ["1L", "5L", "10L", "20L"], //enum restricts the string to one of the specified values
     },
     price: {
       type: Number,
