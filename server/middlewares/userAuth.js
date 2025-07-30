ed3aa8d34258bd6e42afa857509e94fca4a77c0a
kelly brevin <kellybrevin83@gmail.com>
2025-07-30 11:10:30 +0300

    changes

--- server/middlewares/userAuth.js
+++ server/middlewares/userAuth.js
@@ -5,5 +5,12 @@ const userAuth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   //check for bearer token
-  
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    return res.status(401).json({ message: "No token,authorization denied" });
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    //verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {}
 };
