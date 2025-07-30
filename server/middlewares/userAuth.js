b2717cba9e90ab8957c0ba218a1420f5184552f5
kelly brevin <kellybrevin83@gmail.com>
2025-07-30 11:05:15 +0300

    user middleware

--- server/middlewares/userAuth.js
+++ server/middlewares/userAuth.js
@@ -1,4 +1,9 @@
 const jwt = require("jsonwebtoken");
 const user = require("../models/User");
 
-const userAuth = async (req, res, next) => {};
+const userAuth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  //check for bearer token
+  
+};
