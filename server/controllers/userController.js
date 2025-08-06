7cb6f91323a63082cea2413298bb77c607e43e3a
kelly brevin <kellybrevin83@gmail.com>
2025-08-06 23:14:51 +0300

    controller changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,12 +4,10 @@ const user = require("../models/User");
 
 //create a new user
 exports.createUser = async (req, res) => {
+  const { name, email, phoneNumber, password, location } = req.body;
   try {
-    const user = await User.create(req.body);
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+    const existing = await User.findOne({ email });
+  } catch (error) {}
 };
 
 //get all users
