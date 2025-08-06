9ddd3f113c77c2571c6d2ea6a9fe41a17c006763
kelly brevin <kellybrevin83@gmail.com>
2025-08-06 23:17:31 +0300

    controller mods

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -9,6 +9,15 @@ exports.createUser = async (req, res) => {
     const existing = await User.findOne({ email });
     if (existing)
       return res.status(400).json({ message: "user already exists" });
+    //create new user
+
+    const user = await User.create({
+      name,
+      email,
+      phoneNumber,
+      password,
+      location,
+    });
   } catch (error) {}
 };
 
