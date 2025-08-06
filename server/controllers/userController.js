dffa32377ec1379ffdacc07c175999755d9e33d4
kelly brevin <kellybrevin83@gmail.com>
2025-08-06 23:56:14 +0300

    new commits

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,8 +10,8 @@ exports.createUser = async (req, res) => {
     const existing = await User.findOne({ email });
     if (existing)
       return res.status(400).json({ message: "user already exists" });
-    //create new user
 
+    //create new user absed on destructured values
     const user = await User.create({
       name,
       email,
