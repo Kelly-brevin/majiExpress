358ef37d99aaf820aad8ac052fe03d2f5be4e8b4
kelly brevin <kellybrevin83@gmail.com>
2025-08-07 20:57:55 +0300

    new chnages

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,24 +1,9 @@
 const Order = require("../models/Order");
+const Product = require("../models/Product");
 
 //place a new order(user)
 exports.createOrder = async (req, res) => {
-  try {
-    // destructure incoming request to get items and location
-    const { items, location } = req.body;
-
-    const userId = req.user.id; //comes from auth middleware
-
-    const newOrder = await Order.create({
-      user: userId,
-      items,
-      location,
-    });
-    res.status(201).json(newOrder);
-  } catch (error) {
-    res
-      .status(400)
-      .json({ message: "Failed to create order", error: error.message });
-  }
+  
 };
 
 //get users's own orders
