fb6a1f1017eb1d1cb5d0505b664fd0fcd7515646
kelly brevin <kellybrevin83@gmail.com>
2026-01-20 12:12:48 +0300

    react app bootstrapped

---
+++ client/src/pages/auth/Login.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { userLogin } from "../../api/auth.api";
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await userLogin({ email, password });
+    login(res.data);
+    navigate("/");
+  };
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-6 rounded shadow w-80"
+      >
+        <h2 className="text-xl font-bold mb-4">Login</h2>
+
+        <input
+          className="w-full border p-2 mb-3"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          className="w-full border p-2 mb-3"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="w-full bg-blue-600 text-white py-2 rounded">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
